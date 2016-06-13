1a1306f8166bf6f5f7c49ac92c494fb35bf04d38
Giuliano CANEPA <giuliano.canepa@gmail.com>
2016-06-13 11:06:02 +0200

    Commit d'initialisation

---
+++ README.md
@@ -0,0 +1,4 @@
+Symfony3
+========
+
+A Symfony project created on June 2, 2016, 4:24 pm.
---
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace OC\PlatformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdvertController extends Controller
+{
+
+    public function indexAction()
+    {
+        $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'Toto'));
+        return new Response($content);
+    }
+
+}
